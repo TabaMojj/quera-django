000da6ce7e5582361531a4d91b9e9cf35b7e6ecd
Mojtaba Mir Yaghoobzdeh <mirmojtaba21@gmail.com>
2023-08-05 23:12:31 +0330

    feat: add CustomUserAdmin class

--- problemset-129725/jalali/Users/admin.py
+++ problemset-129725/jalali/Users/admin.py
@@ -1,9 +1,19 @@
-from Users.models import Birthday
+from .forms import CustomUserForm
+from .models import CustomUser
 from django.contrib import admin
 
-#from django_jalali.admin.filters import JDateFieldListFilter
-#import django_jalali.admin as jadmin
 
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    @admin.display(description='first name')
+    def first_name(self, obj):
+        return obj.full_name.split()[0]
 
-admin.site.register(Birthday)
+    @admin.display(description='last name')
+    def last_name(self, obj):
+        return obj.full_name.split()[1]
 
+    form = CustomUserForm
+    list_display = ['username', 'first_name', 'last_name', 'gender', 'national_code', 'birthday_date']
+    search_fields = ['username', 'full_name']
+    ordering = ['ceremony_datetime']
