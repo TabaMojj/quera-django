c3084dee78b482516ee83ddacbf73f7e249c79c1
Mojtaba Mir Yaghoobzdeh <mirmojtaba21@gmail.com>
2023-08-06 14:26:38 +0200

    fix: add collectCompany command

--- problemset-102249/commandline/career/management/commands/collectCompany.py
+++ problemset-102249/commandline/career/management/commands/collectCompany.py
@@ -1,5 +1,14 @@
-from django.core.management.base import BaseCommand, CommandError
+import csv
+from django.conf import settings
+from django.core.management.base import BaseCommand
+from career.models import Company
 
 
 class Command(BaseCommand):
-    pass
+    def handle(self, *args, **options):
+        path = f'{settings.BASE_DIR}/company.csv'
+        with open(path, 'w', newline='') as csvfile:
+            writer = csv.writer(csvfile)
+            for obj in Company.objects.all():
+                row = [obj.name, obj.email, obj.phone]
+                writer.writerow(row)
