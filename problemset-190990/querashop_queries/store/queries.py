1eb2960b52fe95aab2adad9bfb79e86006db37f6
Mojtaba Mir Yaghoobzdeh <mirmojtaba21@gmail.com>
2023-08-06 15:53:31 +0200

    initial commit

---
+++ problemset-190990/querashop_queries/store/queries.py
@@ -0,0 +1,25 @@
+# Import here
+
+
+def young_employees(job: str):
+    pass
+
+
+def cheap_products():
+    pass
+
+
+def products_sold_by_companies():
+    pass
+
+
+def sum_of_income(start_date: str, end_date: str):
+    pass
+
+
+def good_customers():
+    pass
+
+
+def nonprofitable_companies():
+    pass
