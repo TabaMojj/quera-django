5d168afdb0c1082c7f0396ee814565959c9831b6
Mojtaba Mir Yaghoobzdeh <mirmojtaba21@gmail.com>
2023-08-08 11:43:05 +0330

    initial commit

---
+++ problemset-129724/Quera_Finance/Finance/queries.py
@@ -0,0 +1,57 @@
+from django.db.models import F, Sum, Count, Case, When
+from .models import *
+
+
+def query_0():
+    q = Employee.objects.all()
+    return q
+
+
+def query_1():
+    # TODO
+    pass
+
+
+def query_2(x):
+    # TODO
+    pass
+
+
+def query_3():
+    # TODO
+    pass
+
+
+def query_4(x):
+    # TODO
+    pass
+
+
+def query_5(x):
+    # TODO
+    pass
+
+
+def query_6():
+    # TODO
+    pass
+
+
+def query_7():
+    # TODO
+    pass
+
+
+def query_8():
+    # TODO
+    pass
+
+
+def query_9(x):
+    # TODO
+    pass
+
+
+def query_10():
+    # TODO
+    pass
