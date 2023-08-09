de0c8ebcd8fceeea4a4e04a2434a7844810a4a8f
Mojtaba Mir Yaghoobzdeh <mirmojtaba21@gmail.com>
2023-08-09 16:05:47 +0200

    feat: add Problem and Submission models

--- problemset-102251/quera_contest/problems/models.py
+++ problemset-102251/quera_contest/problems/models.py
@@ -1,9 +1,17 @@
+from django.conf import settings
 from django.db import models
 
 
 class Problem(models.Model):
-    pass
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=1000)
+    writer = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    score = models.PositiveIntegerField(default=100)
 
 
 class Submission(models.Model):
-    pass
+    submitted_time = models.DateTimeField()
+    participant = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='submissions', on_delete=models.CASCADE)
+    problem = models.ForeignKey(Problem, related_name="submissions", on_delete=models.CASCADE)
+    code = models.URLField(max_length=200)
+    score = models.PositiveIntegerField(default=0)
