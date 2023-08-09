7c3d03999df145033e99d678a40b4dac3b10c93b
Mojtaba Mir Yaghoobzdeh <mirmojtaba21@gmail.com>
2023-08-09 17:16:54 +0200

    initial commit

---
+++ problemset-16401/cabin/cabin/queries.py
@@ -0,0 +1,106 @@
+from .models import *
+
+
+def query_0():
+    q = Driver.objects.all()
+    return q
+
+
+def query_1():
+    q = 'your query here'
+    return q
+
+
+def query_2(x):
+    q = 'your query here'
+    return q
+
+
+def query_3():
+    q = 'your query here'
+    return q
+
+
+def query_4():
+    q = 'your query here'
+    return q
+
+
+def query_5(t):
+    q = 'your query here'
+    return q
+
+
+def query_6():
+    q = 'your query here'
+    return q
+
+
+def query_7():
+    q = 'your query here'
+    return q
+
+
+def query_8(x):
+    q = 'your query here'
+    return q
+
+
+def query_9():
+    q = 'your query here'
+    return q
+
+
+def query_10():
+    q = 'your query here'
+    return q
+
+
+def query_11(n, c):
+    q = 'your query here'
+    return q
+
+
+def query_12(n, c):
+    q = 'your query here'
+    return q
+
+
+def query_13(n, m):
+    q = 'your query here'
+    return q
+
+
+def query_14(x, y, r):
+    q = 'your query here'
+    return q
+
+
+def query_15(n, c):
+    q = 'your query here'
+    return q
+
+
+def query_16(x, t):
+    q = 'your query here'
+    return q
+
+
+def query_17():
+    q = 'your query here'
+    return q
+
+
+def query_18():
+    q = 'your query here'
+    return q
+
+
+def query_19(n, t):
+    q = 'your query here'
+    return q
+
+
+def query_20():
+    q = 'your query here'
+    return q
