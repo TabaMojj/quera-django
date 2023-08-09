c4df8995b4816cbe95fab7dcaf47ea5a1888e406
Mojtaba Mir Yaghoobzdeh <mirmojtaba21@gmail.com>
2023-08-09 22:03:09 +0200

    feat: add queries

--- problemset-16401/cabin/cabin/queries.py
+++ problemset-16401/cabin/cabin/queries.py
@@ -1,3 +1,4 @@
+from django.db.models import Sum, Count, Case, When, F
 from .models import *
 
 
@@ -7,100 +8,97 @@ def query_0():
 
 
 def query_1():
-    q = 'your query here'
-    return q
+    queryset = Payment.objects.aggregate(income=Sum('amount'))
+    return queryset
 
 
 def query_2(x):
-    q = 'your query here'
-    return q
+    queryset = (Payment.objects
+                .filter(ride__request__rider_id=x)
+                .aggregate(payment_sum=Sum('amount')))
+    return queryset
 
 
 def query_3():
-    q = 'your query here'
-    return q
+    queryset = (Driver.objects
+                .filter(car__car_type='A')
+                .annotate(num_cars=Count('car'))
+                .filter(num_cars__gte=1)
+                .count())
+
+    return queryset
 
 
 def query_4():
-    q = 'your query here'
-    return q
+    queryset = RideRequest.objects.filter(ride__isnull=True).all()
+    return queryset
 
 
 def query_5(t):
-    q = 'your query here'
-    return q
+    queryset = (Rider.objects
+                .annotate(total_payments=Sum('riderequest__ride__payment__amount'))
+                .filter(total_payments__gte=t))
+    return queryset
 
 
 def query_6():
-    q = 'your query here'
-    return q
+    queryset = (Account.objects
+                .annotate(num_cars=Count('drivers__car'))
+                .order_by('-num_cars', 'last_name')
+                .first())
+    return queryset
 
 
 def query_7():
-    q = 'your query here'
-    return q
+    queryset = (Rider.objects
+                .filter(riderequest__ride__car__car_type='A')
+                .annotate(n=Count('id')))
+    return queryset
 
 
 def query_8(x):
-    q = 'your query here'
-    return q
+    queryset = (Driver.objects
+                .annotate(car_model_fixed=Case(When(car__model__lte=99, then=1300 + F('car__model')), default=F('car__model')))
+                .filter(car__model__gte=x)
+                .annotate(num=Count('car'))
+                .filter(num__gte=1)
+                .values('account__email'))
+
+    return queryset
 
 
 def query_9():
-    q = 'your query here'
-    return q
+    queryset = (Driver.objects
+                .annotate(n=Count('car__ride')))
+    return queryset
 
 
 def query_10():
-    q = 'your query here'
-    return q
+    queryset = (Driver.objects
+                .values('account__first_name')
+                .annotate(n=Count('car__ride')))
+    return queryset
 
 
 def query_11(n, c):
-    q = 'your query here'
-    return q
+    queryset = (Driver.objects
+                .filter(car__color=c, car__model__gte=n)
+                .distinct())
+    return queryset
 
 
 def query_12(n, c):
-    q = 'your query here'
-    return q
+    queryset = (Driver.objects
+                .filter(car__color=c)
+                .filter(car__model__gte=n)
+                .distinct())
+    return queryset
 
 
 def query_13(n, m):
-    q = 'your query here'
-    return q
+    queryset = (Ride.objects
+                .filter(request__rider__account__first_name=m, car__owner__account__first_name=n)
+                .annotate(duration=F('dropoff_time') - F('pickup_time'))
+                .aggregate(sum_duration=Sum('duration')))
 
-
-def query_14(x, y, r):
-    q = 'your query here'
-    return q
-
-
-def query_15(n, c):
-    q = 'your query here'
-    return q
-
-
-def query_16(x, t):
-    q = 'your query here'
-    return q
-
-
-def query_17():
-    q = 'your query here'
-    return q
-
-
-def query_18():
-    q = 'your query here'
-    return q
-
-
-def query_19(n, t):
-    q = 'your query here'
-    return q
-
-
-def query_20():
-    q = 'your query here'
-    return q
+    return queryset
