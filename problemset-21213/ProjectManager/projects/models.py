07019ebf5ceaebb93cc0bfbb93eb94fe5ebe9afe
Mojtaba Mir Yaghoobzdeh <mirmojtaba21@gmail.com>
2023-08-14 20:23:07 +0330

    feat: add has_permission method to ProjectMembership model

--- problemset-21213/ProjectManager/projects/models.py
+++ problemset-21213/ProjectManager/projects/models.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.models import User
-from django.db import models, transaction
+from django.db import models
 
 
 class Project(models.Model):
@@ -22,6 +22,21 @@ class ProjectMembership(models.Model):
 
     )
 
+    PERMISSIONS = {
+        'create_new_issue': [ROLE_GUEST, ROLE_REPORTER, ROLE_DEVELOPER, ROLE_MASTER, ROLE_OWNER],
+        'leave_comments': [ROLE_GUEST, ROLE_REPORTER, ROLE_DEVELOPER, ROLE_MASTER, ROLE_OWNER],
+        'pull_project_code': [ROLE_REPORTER, ROLE_DEVELOPER, ROLE_MASTER, ROLE_OWNER],
+        'assign_issues_and_merge_requests': [ROLE_REPORTER, ROLE_DEVELOPER, ROLE_MASTER, ROLE_OWNER],
+        'see_a_list_of_merge_requests': [ROLE_REPORTER, ROLE_DEVELOPER, ROLE_MASTER, ROLE_OWNER],
+        'manage_merge_requests': [ROLE_DEVELOPER, ROLE_MASTER, ROLE_OWNER],
+        'create_new_branches': [ROLE_DEVELOPER, ROLE_MASTER, ROLE_OWNER],
+        'add_new_team_members': [ROLE_MASTER, ROLE_OWNER],
+        'push_to_protected_branches': [ROLE_MASTER, ROLE_OWNER],
+        'switch_visibility_level': [ROLE_OWNER],
+        'remove_project': [ROLE_OWNER],
+        'force_push_to_protected_branches': [],
+    }
+
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     role = models.CharField(max_length=4, choices=ROLE_CHOICES, default=ROLE_GUEST, verbose_name='Role')
@@ -30,4 +45,8 @@ class ProjectMembership(models.Model):
     class Meta:
         unique_together = ('user', 'project')
 
+    def has_permission(self, action):
+        if self.role in self.PERMISSIONS.get(action, []):
+            return True
+        return False
 
