5744bea75b4e68cc3041f7151ea8d7b857ead6c2
Mojtaba Mir Yaghoobzdeh <mirmojtaba21@gmail.com>
2023-08-16 22:33:41 +0330

    feat: add admin search fields and permissions

--- problemset-76279/boors-va-corona/apps/guard/admin.py
+++ problemset-76279/boors-va-corona/apps/guard/admin.py
@@ -8,22 +8,31 @@ from .models import BlockedIp, SecurityConfig, ViewDetail
 @admin.register(BlockedIp)
 class BlockedIpAdmin(admin.ModelAdmin):
     list_display = ('id', 'ip', 'rps', 'view', 'ban_time', 'created_at')
-
-    # Your code
-
+    list_filter = ('view',)
+    search_fields = ('ip', 'view__name', 'view__path')
     list_display_links = ('id', 'ip')
 
 
 @admin.register(SecurityConfig)
 class SecurityConfigAdmin(admin.ModelAdmin):
-    # Your code
-    pass
+    def has_add_permission(self, request):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
 
 
 @admin.register(ViewDetail)
 class ViewDetailAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'path')
-
     search_fields = ('name', 'path')
 
-    # Your code
+    def has_add_permission(self, request):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
