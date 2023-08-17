35ef69baa483ce13ea4845f33bbf86af590c0e04
Mojtaba Mir Yaghoobzdeh <mirmojtaba21@gmail.com>
2023-08-17 15:48:54 +0330

    feat: add middlewares module

--- problemset-76279/boors-va-corona/apps/guard/middlewares.py
+++ problemset-76279/boors-va-corona/apps/guard/middlewares.py
@@ -1,12 +1,9 @@
+from datetime import timedelta
 from django.http import JsonResponse
-
 from django.core.cache import cache
-from django.urls import resolve
 from django.utils import timezone
 from rest_framework import status
-
-from .models import BlockedIp, SecurityConfig, ViewDetail
-
+from .models import BlockedIp, ViewDetail
 from .utils import get_client_ip
 
 
@@ -18,31 +15,36 @@ class BlockIpMiddleware:
         self.get_response = get_response
 
     def __call__(self, request):
-        view, _, _ = resolve(request.path)
         user_ip = get_client_ip(request)
+        path = request.path
+        default_cache_value = {'count': 0, 'last_updated': timezone.now()}
 
-        response = self.banned_before(user_ip)
-        if response:
-            return response
+        if self.banned_before(user_ip):
+            return JsonResponse(data={'message': 'Forbidden'}, status=status.HTTP_403_FORBIDDEN)
 
-        LIMITED_VIEWS = SecurityConfig.objects.last().views.values_list(
-            'name',
-            flat=True)
-
-        # Your code
+        requests = cache.get(user_ip) or default_cache_value
+        if self.validate_request_per_second(requests=requests, user_ip=user_ip, path=path):
+            return JsonResponse({'message': 'Too many requests'}, status=status.HTTP_429_TOO_MANY_REQUESTS)
 
         response = self.get_response(request)
-
+        cache.set(user_ip, requests, timeout=self.timeout)
         return response
 
-        pass
-
     def banned_before(self, user_ip):
+        blocked_ip = BlockedIp.objects.filter(ip=user_ip)
+        if blocked_ip.exists() and blocked_ip.get().is_blocked:
+            return True
+        return False
 
-        # Your code
-        pass
-
-    def validate_request_per_second(self, user_ip, url_path, data):
-
-        # Your code
-        pass
+    def validate_request_per_second(self, user_ip, requests, path):
+        requests['count'] += 1
+        requests['last_updated'] = timezone.now()
+        count = requests['count']
+        now = requests['last_updated']
+        delta = now - timezone.now()
+        if count > self.maximum_rps and delta < timedelta(seconds=1):
+            view = ViewDetail.objects.get(path=path)
+            BlockedIp.objects.create(ip=user_ip, view=view, rps=self.maximum_rps)
+            return True
+        cache.set(user_ip, requests)
+        return False
