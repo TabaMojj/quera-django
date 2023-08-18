a25342737df5cb98171c92f9f8dc3f3561d5c442
Mojtaba Mir Yaghoobzdeh <mirmojtaba21@gmail.com>
2023-08-18 21:20:56 +0330

    feat: add permissions

--- problemset-21212/Blog/app/permissions.py
+++ problemset-21212/Blog/app/permissions.py
@@ -2,4 +2,9 @@ from rest_framework import permissions
 
 
 class IsOwnerOrAdminOrReadOnly(permissions.BasePermission):
-    pass
+    def has_object_permission(self, request, view, obj):
+        if request.method == 'GET':
+            return True
+        elif request.user.is_superuser or request.user == obj.owner:
+            return True
+        return False
