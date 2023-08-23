5a118a5bcc35801b0cdbeae1a3b98675b75d5cb8
Mojtaba Mir Yaghoobzdeh <mirmojtaba21@gmail.com>
2023-08-23 09:59:17 +0330

    feat: add BookSerializer

--- problemset-175887/discounts/books/serializers.py
+++ problemset-175887/discounts/books/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from rest_framework.reverse import reverse
 
 from .models import Book, Category
 
@@ -9,4 +10,25 @@ class CategorySerializer(serializers.ModelSerializer):
         fields = ('id', 'name')
         read_only_fields = ('id',)
 
-# TODO: write serializer/serializers here
+
+class BookSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Book
+        fields = '__all__'
+
+    categories = serializers.PrimaryKeyRelatedField(many=True, queryset=Category.objects.all())
+    price_after_discount = serializers.SerializerMethodField()
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        categories = representation.pop('categories')
+        category_urls = [reverse('books:category-detail',
+                                 kwargs={'pk': category},
+                                 request=self.context['request'])
+                         for category in categories]
+        representation['categories'] = category_urls
+        return representation
+
+    def get_price_after_discount(self, obj):
+        request = self.context.get('request')
+        return obj.get_discount(request.user)
