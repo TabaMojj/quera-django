f73e6c0f86a04ea071e2e38892036a3014f2ad46
Mojtaba Mir Yaghoobzdeh <mirmojtaba21@gmail.com>
2023-08-26 23:24:41 +0330

    fix: remove redundant code

--- problemset-175888/upload_center/upload_center/serializers.py
+++ problemset-175888/upload_center/upload_center/serializers.py
@@ -1,171 +1,20 @@
-import os
-import uuid
-from _decimal import Decimal
-from pathlib import Path
-
-from django.conf import settings
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
 from django.core.files.storage import default_storage
-from rest_framework.exceptions import ValidationError
-import decimal
 
 User = get_user_model()
 
 
-def get_megabytes(value): #TODO: WRITE ABOUT THIS IN README
+def get_megabytes(value):  # TODO: WRITE ABOUT THIS IN README
     megabytes = value / (1024 * 1024)
     value = f'{megabytes:.3f}'
     return value
 
 
-def printknapSack(user_available_storage, files):
-    chosen_files = []
-    user_available_storage = int(float(get_megabytes(user_available_storage)))
-    n = len(files)
-    val = [1] * n
-    K = [[0 for w in range(user_available_storage + 1)]
-         for i in range(n + 1)]
-    for i in range(n + 1):
-        for w in range(user_available_storage + 1):
-            if i == 0 or w == 0:
-                K[i][w] = 0
-            elif int(float(get_megabytes(files[i - 1].size))) <= w:
-                K[i][w] = max(val[i - 1]
-                              + K[i - 1][w - int(float(get_megabytes(files[i - 1].size)))],
-                              K[i - 1][w])
-            else:
-                K[i][w] = K[i - 1][w]
-    res = K[n][user_available_storage]
-    w = user_available_storage
-    for i in range(n, 0, -1):
-        if res <= 0:
-            break
-        if res == K[i - 1][w]:
-            continue
-        else:
-            chosen_files.append(files[i - 1])
-            res = res - val[i - 1]
-            w = w - int(float(get_megabytes(files[i - 1].size)))
-    return chosen_files
-
-
-
-
-
 class UploadFilesSerializer(serializers.Serializer):
-    # file_field = serializers.FileField(allow_empty_file=False, required=True, use_url=False)
-    #
     file_field = serializers.ListField(
-        child=serializers.FileField(use_url=False, allow_empty_file=False, allow_null=False),
+        child=serializers.FileField(allow_empty_file=False, allow_null=False),
         allow_null=False, allow_empty=False)
-
-    # def create(self, validated_data):
-    #     user = self.context['user']
-    #     files = validated_data['file_field']
-    #     saved_files_result = {}
-    #     number_of_files = len(files)
-    #     if number_of_files == 1 and files[0].size == 0:
-    #         raise ValidationError('EMPTY !!')
-    #
-    #     uploaded_files_names = [file.name for file in files]
-    #     jjson = {}
-    #     for name in uploaded_files_names:
-    #         if name in jjson:
-    #             jjson[name] += 1
-    #         else:
-    #             jjson[name] = 1
-    #
-    #     for file in files:
-    #         if jjson[file.name] > 1:
-    #             jjson[file.name] -= 1
-    #             path = Path(file.name)
-    #             file.name = path.with_name(path.stem + '-' + str(uuid.uuid4()) + path.suffix).name
-    #     files_permitted_to_upload = []
-    #     for file in files:
-    #         if file.size > user.account.max_file_transfer:
-    #             user_max_file_transfer_megabytes = get_megabytes(user.account.max_file_transfer)
-    #             message = f'You can\'t upload files more than {user_max_file_transfer_megabytes} Megabytes!'
-    #             saved_files_result[file.name] = message
-    #         else:
-    #             files_permitted_to_upload.append(file)
-    #
-    #     user_current_available_storage = user.account.storage - user.used_storage
-    #     files_to_upload = printknapSack(user_current_available_storage, files_permitted_to_upload)
-    #     for f in files_to_upload:
-    #         if f in files_permitted_to_upload:
-    #             files_permitted_to_upload.remove(f)
-    #     for f in files_permitted_to_upload:
-    #         message = 'You don\'t have enough space to upload this file!'
-    #         saved_files_result[f.name] = message
-    #     for file in files_to_upload:
-    #         if file.size == 0:
-    #             continue
-    #         # file_directory = default_storage.generate_filename(f'{user.username}/{file.name}')
-    #         # filename = file_directory.split('/')[1]
-    #
-    #         save = os.path.join(settings.BASE_DIR, settings.MEDIA_ROOT, user.username, file.name)
-    #         filepath = default_storage.save(save, file.file)
-    #         filename = filepath.split('/')[1]
-    #         user.used_storage += file.size
-    #         user.save()
-    #         url = default_storage.url(filepath)
-    #         saved_files_result[filename] = url
-    #     return saved_files_result
-
-    # def save(self, **kwargs):
-    # user = self.context['user']
-    # files = self.validated_data['file_field']
-    # saved_files_result = {}
-    # number_of_files = len(files)
-    # if number_of_files == 1 and files[0].size == 0:
-    #     raise ValidationError('EMPTY !!')
-    #
-    # uploaded_files_names = [file.name for file in files]
-    # jjson = {}
-    # for name in uploaded_files_names:
-    #     if name in jjson:
-    #         jjson[name] += 1
-    #     else:
-    #         jjson[name] = 1
-    #
-    # for file in files:
-    #     if jjson[file.name] > 1:
-    #         jjson[file.name] -= 1
-    #         path = Path(file.name)
-    #         file.name = path.with_name(path.stem + '-' + str(uuid.uuid4()) + path.suffix).name
-    # files_permitted_to_upload = []
-    # for file in files:
-    #     if file.size > user.account.max_file_transfer:
-    #         user_max_file_transfer_megabytes = get_megabytes(user.account.max_file_transfer)
-    #         message = f'You can\'t upload files more than {user_max_file_transfer_megabytes} Megabytes!'
-    #         saved_files_result[file.name] = message
-    #     else:
-    #         files_permitted_to_upload.append(file)
-    #
-    # user_current_available_storage = user.account.storage - user.used_storage
-    # files_to_upload = printknapSack(user_current_available_storage, files_permitted_to_upload)
-    # for f in files_to_upload:
-    #     if f in files_permitted_to_upload:
-    #         files_permitted_to_upload.remove(f)
-    # for f in files_permitted_to_upload:
-    #     message = 'You don\'t have enough space to upload this file!'
-    #     saved_files_result[f.name] = message
-    # for file in files_to_upload:
-    #     if file.size == 0:
-    #         continue
-    #     # file_directory = default_storage.generate_filename(f'{user.username}/{file.name}')
-    #     # filename = file_directory.split('/')[1]
-    #
-    #     save = os.path.join(settings.BASE_DIR, settings.MEDIA_ROOT, user.username, file.name)
-    #     print(save)
-    #     filepath = default_storage.save(save, file.file)
-    #     filename = filepath.split('/')[1]
-    #     user.used_storage += file.size
-    #     user.save()
-    #     url = default_storage.url(filepath)
-    #     saved_files_result[filename] = url
-    # return saved_files_result
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -186,8 +35,7 @@ class UserSerializer(serializers.ModelSerializer):
 
     def get_files(self, obj):
         username = obj.username
-        is_username_folder_exists = default_storage.exists(name=username)
-        if is_username_folder_exists:
+        if default_storage.exists(name=username):
             user_files = default_storage.listdir(path=username)[1]
             if user_files:
                 return user_files
--- problemset-175888/upload_center/upload_center/views.py
+++ problemset-175888/upload_center/upload_center/views.py
@@ -2,14 +2,13 @@ import os
 import uuid
 
 from django.conf import settings
-from rest_framework.exceptions import ValidationError
 from rest_framework.generics import GenericAPIView
-from rest_framework.parsers import MultiPartParser, FileUploadParser
+from rest_framework.parsers import MultiPartParser
 from rest_framework.response import Response
 from django.http.response import FileResponse
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework import status
-from .serializers import UploadFilesSerializer, UserSerializer, get_megabytes, printknapSack
+from .serializers import UploadFilesSerializer, UserSerializer, get_megabytes
 from django.core.files.storage import default_storage
 from pathlib import Path
 
@@ -20,83 +19,22 @@ class UploadFile(GenericAPIView):
     parser_classes = [MultiPartParser]
 
     def put(self, request):
-        serializer = UploadFilesSerializer(data=request.data) # context={'user': request.user}
+        serializer = UploadFilesSerializer(data=request.data)
         if serializer.is_valid(raise_exception=False):
-
             files = serializer.validated_data['file_field']
             user = request.user
             saved_files_result = {}
-
-            # uploaded_files_names = [file.name for file in files]
-            # jjson = {}
-            # for name in uploaded_files_names:
-            #     if name in jjson:
-            #         jjson[name] += 1
-            #     else:
-            #         jjson[name] = 1
-            #
-            # for file in files:
-            #     if jjson[file.name] > 1:
-            #         jjson[file.name] -= 1
-            #         path = Path(file.name)
-            #         file.name = path.with_name(path.stem + '-' + str(uuid.uuid4()) + path.suffix).name
-            # files_permitted_to_upload = []
-            # for file in files:
-            #     if file.size > user.account.max_file_transfer:
-            #         user_max_file_transfer_megabytes = get_megabytes(user.account.max_file_transfer)
-            #         message = f'You can\'t upload files more than {user_max_file_transfer_megabytes} Megabytes!'
-            #         saved_files_result[file.name] = message
-            #     else:
-            #         files_permitted_to_upload.append(file)
-
-            # user_current_available_storage = user.account.storage - user.used_storage
-            # files_to_upload = []
-            # current_storage_used = 0
-            files_permitted_to_upload_sorted = sorted(files, key=lambda x: x.size, reverse=False)
-
-            # for file in files_permitted_to_upload_sorted:
-            #     if current_storage_used + file.size <= user_current_available_storage:
-            #         current_storage_used += file.size
-            #         files_to_upload.append(file)
-            #     else:
-            #         message = 'You don\'t have enough space to upload this file!'
-            #         saved_files_result[file.name] = message
-            #
-            # files_to_upload = printknapSack(user_current_available_storage, files_permitted_to_upload)
-
-            # for f in files_to_upload:
-            #     print(f.size)
-            # for f in files_to_upload:
-            #     if f in files_permitted_to_upload:
-            #         files_permitted_to_upload.remove(f)
-            # for f in files_permitted_to_upload:
-            #     message = 'You don\'t have enough space to upload this file!'
-            #     saved_files_result[f.name] = message
-            for file in files_permitted_to_upload_sorted:
-                if file.size > user.account.max_file_transfer:
-                    user_max_file_transfer_megabytes = get_megabytes(user.account.max_file_transfer)
-                    message = f'You can\'t upload files more than {user_max_file_transfer_megabytes} Megabytes!'
-                    saved_files_result[file.name] = message
-                    continue
-                if file.size + user.used_storage > user.account.storage:
-                    message = 'You don\'t have enough space to upload this file!'
-                    saved_files_result[file.name] = message
-                    continue
+            max_file_transfer_message = f'You can\'t upload files more than {get_megabytes(user.account.max_file_transfer)} Megabytes!'
+            not_enough_storage_message = 'You don\'t have enough space to upload this file!'
+            can_upload = sorted(files, key=lambda x: x.size)
+            for file in can_upload:
                 if file.name in saved_files_result:
                     path = Path(file.name)
                     file.name = path.with_name(path.stem + '-' + str(uuid.uuid4()) + path.suffix).name
-
-                # if file.name in saved_files_result:
-                #     path = Path(file.name)
-                #     file.name = path.with_name(path.stem + '-' + str(uuid.uuid4()) + path.suffix).name
-                # if file.size + user.used_storage >= user.account.storage:
-                #     message = 'You don\'t have enough space to upload this file!'
-                #     saved_files_result[file.name] = message
-                #     continue
                 save = os.path.join(settings.BASE_DIR, settings.MEDIA_ROOT, user.username, file.name)
                 filepath = default_storage.save(save, file)
                 filename = filepath.split('/')[1]
-                user.used_storage += file.size
+                user.used_storage = user.used_storage + file.size
                 user.save()
                 url = default_storage.url(filepath)
                 saved_files_result[filename] = url
